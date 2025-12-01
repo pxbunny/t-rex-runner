d0a4397628eee5e2681663d4e47e20162850fec1
pxbunny <rafalcz29@gmail.com>
2025-12-01 11:46:55 +0100

    Code formatting

--- source/main.cpp
+++ source/main.cpp
@@ -4,33 +4,24 @@
 #include "game.hpp"
 
 Config config = {
-
     // WINDOW //
-
     "T-Rex Runner",     // title
     600u,               // width
     200u,               // height
     10u,                // padding
     60u,                // framerate limit
 
-
     // COLORS //
-
     32u,                // bits per pixel
 
-
     // GAME //
-
     40.f,               // dinosaur X position
     180.f,              // ground level (y position)
     10.f,               // initial jump speed
     .4f,                // gravitational acceleration
 
-
     // PATHS //
-
     "assets/font.ttf",  // font path
-    
 };
 
 int main()
